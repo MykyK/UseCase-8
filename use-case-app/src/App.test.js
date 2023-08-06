f7fafcc12952cf573712088c8111cd088fab489b
MYKYTA KALIUZHNYI -X (mkaliuzh - SOFTSERVE INC at Cisco) <mkaliuzh@cisco.com>
2023-08-06 19:06:37 +0300

    UC#8 Unit test(fix sonarcube warnings)

--- use-case-app/src/App.test.js
+++ use-case-app/src/App.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import reducer from './store/reducers';
